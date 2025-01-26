c33e683efbcce6d75a207a34022d7ba94f92b197
yarfill <fill3272@gmail.com>
2025-01-26 18:20:11 -0500

    My first javascript lesson

---
+++ previousLessonsPart1/1.9ValuesAndVeriables.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 -10);
+
+console.log("Phill");
+console.log("25");
+
+let firstName = "Phill";
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
