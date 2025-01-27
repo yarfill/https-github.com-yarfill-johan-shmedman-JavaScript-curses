a5816bcd90c646ab0249fa24ecfe136f9013b162
yarfill <fill3272@gmail.com>
2025-01-27 11:44:54 -0500

    Let const and var in JavaScript

---
+++ previousLessonsPart1/1.12LetConstAndVar.js
@@ -0,0 +1,12 @@
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'developer';
+
+lastName = 'Filatov';
+console.log(lastName);
