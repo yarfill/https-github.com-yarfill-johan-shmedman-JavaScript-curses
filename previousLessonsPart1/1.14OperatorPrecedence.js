987095abe701c38f69864717de56cd35364f8311
yarfill <fill3272@gmail.com>
2025-01-27 13:38:00 -0500

    Operator Precedence lesson in JavaScript

---
+++ previousLessonsPart1/1.14OperatorPrecedence.js
@@ -0,0 +1,14 @@
+
+const now = 2037;
+const agePhill = now - 1999;
+const ageSarah = now - 2015;
+
+console.log(now - 1999 > now - 2020);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (agePhill + ageSarah) / 2
+console.log("agePhill =", agePhill, ageSarah, averageAge);
+ 
