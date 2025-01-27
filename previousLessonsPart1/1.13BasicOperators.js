46032f2b1381cb6aee92eda46a09c8cdc97822cb
yarfill <fill3272@gmail.com>
2025-01-27 13:31:08 -0500

    Basic Operators in JavaScript

---
+++ previousLessonsPart1/1.13BasicOperators.js
@@ -0,0 +1,33 @@
+// Basic
+
+lastName = 'Filatov';
+console.log(lastName);
+
+const now = 2037;
+const agePhill = now - 1999;
+const ageSarah = now - 2015;
+console.log(agePhill, ageSarah);
+console.log( agePhill * 2, agePhill / 10, 2 ** 3); 
+
+const firstName = 'Phill';
+const lastName = 'Filatov';
+console.log(firstName + ' ' + lastName);
+
+//Assigment operators
+
+let x = 10 + 5; //x = 15!
+x += 10; //x = x + 10 = 25!
+x *= 4;//x = x *4 = 100;
+x++; // x = x + 1 = 101;
+x--; // x = x - 1 = 99;
+console.log(x);
+
+//Comparison operators
+
+console.log(agePhill > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1999 > now - 2020);
+
