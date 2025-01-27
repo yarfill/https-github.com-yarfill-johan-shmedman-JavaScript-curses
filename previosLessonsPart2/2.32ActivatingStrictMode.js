f13bef07ed66acbc86250d3c62c691cf7cea3b68
yarfill <fill3272@gmail.com>
2025-01-27 16:20:27 -0500

    Activating Strict Mode in JS

---
+++ previosLessonsPart2/2.32ActivatingStrictMode.js
@@ -0,0 +1,10 @@
+'use strict';// Temporarily disabled for debugging. So if you has a mistake in javascript this mode will help you to found it.
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriverLicence = true; // !It's a mistake for mode 'strict'!
+if (hasDriversLicence) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 534;
